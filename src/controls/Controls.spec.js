360085e19eeb123fe9ed7647e08ab2634abaeda7
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-11 18:30:37 -0500

    got the display tests to work

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,7 @@
-// Test away!
+import React from 'react';
+import Controls from "./Controls";
+import * as yeet from "@testing-library/react";
+
+test("control rendered", ()=>{
+    expect(yeet.render(<Controls/>)).toMatchSnapshot();
+})
