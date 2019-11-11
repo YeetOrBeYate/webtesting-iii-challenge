8a622bf98731b0d2f86f8937b018b054d365e8c6
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-11 17:26:37 -0500

    got the first dashboard test done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import React from 'react';
+import Dashboard from "./Dashboard";
+import * as yeet from "@testing-library/react";
+
+
+test("shows the controls and display", ()=>{
+    expect(yeet.render(<Dashboard/>)).toMatchSnapshot();
+})
