60a659cf13687e3ad445769ce3437af27848b235
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-11 20:18:05 -0500

    working on control

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Controls from "./Controls";
 import * as yeet from "@testing-library/react";
+import { fireEvent } from '@testing-library/react';
 
 test("control rendered", ()=>{
     expect(yeet.render(<Controls/>)).toMatchSnapshot();
+})
+
+test("Close button triggers toggle close",   ()=>{
+    const toggleClosedMock = jest.fn();
+    
+    const {getByText, findByText} = yeet.render(<Controls toggleClosed ={toggleClosedMock}/>)
+
+    const button =  getByText(/close gate/i);
+
+    fireEvent.click(button);
+
+    expect(toggleClosedMock).toHaveBeenCalled();
+    
+     expect(findByText(/open gate/i))
+    
 })
